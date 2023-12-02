0255810f7b75b89707b3967765b433761af9172e
Annika Paulheim <annika.paulheim@gmx.de>
2023-12-02 10:53:58 +0100

    day 2

--- js/day2.js
+++ js/day2.js
@@ -4,41 +4,40 @@ const { parse } = require("path");
 const input = fs.readFileSync("day2.txt", { encoding: "utf8" });
 const lines = input.split("\r\n");
 const games = lines.map((line) => {
-  let [num, setsStr] = line.split(": ");
-  let sets = setsStr.split("; ").map((set) => {
-    let cubes = set.split(", ").map((cube) => {
+  let [_, setsStr] = line.split(": ");
+  return setsStr.split("; ").map((set) =>
+    set.split(", ").map((cube) => {
       let [num, color] = cube.split(" ");
       return { color, num: parseInt(num) };
-    });
-    return cubes;
-  });
-  return sets;
+    })
+  );
 });
-let silver = 0;
-games.forEach((game, index) => {
-  const valid = game.every((set) => {
-    return set.every((cube) => {
-      switch (cube.color) {
-        case "red":
-          return cube.num <= 12;
-        case "green":
-          return cube.num <= 13;
-        case "blue":
-          return cube.num <= 14;
-        default:
-          console.warn("parsing error");
-          return false;
-      }
-    });
-  });
-  if (valid) {
-    silver += index + 1;
-  }
-});
-console.log(silver);
 
-let gold = 0;
-games.forEach((game, index) => {
+let silver = games.reduce(
+  (acc, game, index) =>
+    acc +
+    (game.every((set) =>
+      set.every((cube) => {
+        switch (cube.color) {
+          case "red":
+            return cube.num <= 12;
+          case "green":
+            return cube.num <= 13;
+          case "blue":
+            return cube.num <= 14;
+          default:
+            console.warn("parsing error");
+            return false;
+        }
+      })
+    )
+      ? index + 1
+      : 0),
+  0
+);
+console.log("silver", silver); // 2416
+
+let gold = games.reduce((acc, game) => {
   let red = 0;
   let green = 0;
   let blue = 0;
@@ -59,6 +58,6 @@ games.forEach((game, index) => {
       }
     });
   });
-  gold += red * green * blue;
-});
-console.log("gold", gold);
+  return acc + red * green * blue;
+}, 0);
+console.log("gold", gold); // 63307
